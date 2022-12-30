f19b476b34ce6a704a499c105057668da1eef640
yoannbdn <108993527+yoannbdn@users.noreply.github.com>
2022-12-30 10:30:00 +0100

    Calcjs

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 let tarif = document.getElementById('tarif')
+let food = document.getElementById('foodTVA')
+let alcool = document.getElementById('alcoholTVA')
+
+
 let tarifEntry = 0
+let foodEntry = false
+let alcoholEntry = false
+
+
 tarif.addEventListener('input', function() {
     tarifEntry = this.value
 })
+
+food.addEventListener('check', function(){
+    foodEntry = true
+})
+
+alcool.addEventListener('check', function(){
+    alcoholEntry = true
+})
+
+function calculator () {
+    if (foodEntry == true) {
+        tarifEntry = tarifEntry * 1,055
+    } else if (alcoholEntry == true){
+        tarifEntry = tarifEntry * 1,20
+    }
+}
